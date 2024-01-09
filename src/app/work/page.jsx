b5d31c0bd03d3c8b25f5d562ab97eaaee99595ce
GenuineMarketing <info@genuinemarketing.se>
2024-01-09 17:35:50 +0100

    Removal of text

--- src/app/work/page.jsx
+++ src/app/work/page.jsx
@@ -10,8 +10,7 @@ const WorkPage = () => {
       >
         <p>
           We believe in efficiency and maximizing our resources to provide the
-          best value to our clients. The primary way we do that is by re-using
-          the same five projects we’ve been developing for the past decade.
+          best value to our clients.
         </p>
       </PageIntro>
     </>
