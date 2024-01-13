444af561fcc701e3f66a4456ff74f073a891a67d
GenuineMarketing <info@genuinemarketing.se>
2024-01-13 15:02:03 +0100

    Change of text

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -15,13 +15,13 @@ const AboutPage = () => {
         </p>
         <div className="mt-10 max-w-2xl space-y-6 text-base">
           <p>
-            Studio was started by three friends who noticed that developer
-            studios were charging clients double what an in-house team would
+            Genuine Marketing was started by two friends who noticed that digital agencies
+            were charging clients double what an in-house team would
             cost. Since the beginning, we have been committed to doing things
-            differently by charging triple instead.
+            differently by charging half instead.
           </p>
           <p>
-            At Studio, we’re more than just colleagues — we’re a family. This
+            At Genuine Marketing, we’re more than just colleagues — we’re a family. This
             means we pay very little and expect people to work late. We want our
             employees to bring their whole selves to work. In return, we just
             ask that they keep themselves there until at least 6:30pm.
@@ -30,9 +30,9 @@ const AboutPage = () => {
       </PageIntro>
       <Container className="mt-16">
         <StatList>
-          <StatListItem value="35" label="Underpaid employees" />
-          <StatListItem value="52" label="Placated clients" />
-          <StatListItem value="$25M" label="Invoices billed" />
+          <StatListItem value="8" label="Skilled employees" />
+          <StatListItem value="19" label="Satisfied clients" />
+          <StatListItem value="€5M" label="Invoices billed" />
         </StatList>
       </Container>
       <Cultures />
