61b809ed1c0bf23dffed3c5d8969199bb9271a17
GenuineMarketing <info@genuinemarketing.se>
2024-06-25 12:49:38 +0200

    Build fix

--- src/components/Build.jsx
+++ src/components/Build.jsx
@@ -9,30 +9,21 @@ const Build = () => {
       <div className="space-y-6 text-base text-neutral-600">
         <p>
           Based off of the discovery phase, we develop a comprehensive roadmap
-          for each product and start working towards delivery. The roadmap is an
-          intricately tangled mess of technical nonsense designed to drag the
-          project out as long as possible.
+          for each product and start working towards delivery. The roadmap is an visual representation of the strategy plans. It ties together the strategy for the "why", the work we will need to do to achieve the goals, the "what" and a timeline for completion, the "when".
         </p>
         <p>
           Each client is assigned a key account manager to keep lines of
-          communication open and obscure the actual progress of the project.
-          They act as a buffer between the client’s incessant nagging and the
-          development team who are hard at work scouring open source projects
-          for code to re-purpose.
+          communication open and to be up to date with the progress of the project.
         </p>
         <p>
-          Our account managers are trained to only reply to client emails after
-          9pm, several days after the initial email. This reinforces the general
-          aura that we are very busy and dissuades clients from asking for
-          changes.
+        Our account managers are trained to provide timely and thoughtful responses to client inquiries, ensuring each communication reflects our dedication to quality and client satisfaction.
         </p>
       </div>
       <Blockquote
-        author={{ name: "Debra Fiscal", role: "CEO of Unseal" }}
+        author={{ name: "Emily Birkin", role: "Head of opeations on IKEA" }}
         className="mt-12"
       >
-        Studio_clone were so regular with their progress updates we almost began
-        to think they were automated!
+        Genuine Marketing provided progress updates so consistently that we almost started to believe they were automated!
       </Blockquote>
     </Section>
   );
