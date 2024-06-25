25ce8da7bdb704195279d9c780402ad60c290bbf
GenuineMarketing <info@genuinemarketing.se>
2024-06-25 12:49:22 +0200

    Discover fix

--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -10,34 +10,28 @@ const Discover = () => {
         <p>
           We work closely with our clients to understand their{" "}
           <strong className="font-semibold text-neutral-950">needs</strong> and
-          goals, embedding ourselves in their every day operations to understand
+          <strong className="font-semibold text-neutral-950"> goals</strong>, embedding ourselves in their every day operations to understand
           what makes their business tick.
         </p>
         <p>
-          Our team of private investigators shadow the company director’s for
-          several weeks while our account managers focus on going through their
-          trash. Our senior security experts then perform social engineering
-          hacks to gain access to their{" "}
-          <strong className="font-semibold text-neutral-950">business</strong>
-          accounts — handing that information over to our forensic accounting
-          team.
+        The project discovery phase sets the stage for success by providing a clear roadmap, mitigating risks, and aligning stakeholders expectations. Investing time and effort in this phase pays dividends in the form of smoother project execution and ultimately, a successful outcome. Well-begun project is half-done!
         </p>
         <p>
           Once the full audit is complete, we report back with a comprehensive
-          <strong className="font-semibold text-neutral-950">plan</strong> and,
-          more importantly, a budget.
+          <strong className="font-semibold text-neutral-950"> plan</strong> and
+          more importantly, a <strong className="font-semibold text-neutral-950"> budget</strong>.
         </p>
       </div>
       <h3 className="mt-12 font-display text-base font-semibold text-neutral-950">
         Included in this phase
       </h3>
       <TagList className="mt-4">
-        <TagListItem>In-depth questionnaires</TagListItem>
-        <TagListItem>Feasibility studies</TagListItem>
-        <TagListItem>Blood samples</TagListItem>
-        <TagListItem>Employee surveys</TagListItem>
-        <TagListItem>Proofs-of-concept</TagListItem>
-        <TagListItem>Forensic audit</TagListItem>
+        <TagListItem>Clear goals</TagListItem>
+        <TagListItem>Dealing with risks</TagListItem>
+        <TagListItem>Using resources wisely</TagListItem>
+        <TagListItem>Project research</TagListItem>
+        <TagListItem>Analyzing market trends</TagListItem>
+        <TagListItem>Timeline and budget</TagListItem>
       </TagList>
     </Section>
   );
