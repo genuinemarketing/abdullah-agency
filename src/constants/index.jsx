f718f371a0fec2348203812f5c4155ce7df753f7
GenuineMarketing <info@genuinemarketing.se>
2024-08-19 16:29:05 +0200

    Commented out nav work

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,22 +1,22 @@
 import { SocialMediaProfiles } from "@/components/SocialMedia";
 
 export const navigation = [
-  {
-    title: "Work",
-    links: [
-      { title: "IKEA", href: "/work/ikea" },
-      { title: "MAX", href: "/work/max" },
-      { title: "ERICSSON", href: "/work/ericsson" },
-      {
-        title: (
-          <>
-            See all <span aria-hidden="true">&rarr;</span>
-          </>
-        ),
-        href: "/work",
-      },
-    ],
-  },
+  // {
+  //   title: "Work",
+  //   links: [
+  //     { title: "IKEA", href: "/work/ikea" },
+  //     { title: "MAX", href: "/work/max" },
+  //     { title: "ERICSSON", href: "/work/ericsson" },
+  //     {
+  //       title: (
+  //         <>
+  //           See all <span aria-hidden="true">&rarr;</span>
+  //         </>
+  //       ),
+  //       href: "/work",
+  //     },
+  //   ],
+  // },
   {
     title: "Company",
     links: [
